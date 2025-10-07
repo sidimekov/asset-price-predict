7631ed36aeae7bb5114f6d9c1fd161076eb3c553
studyStudywork <study@MacBook-Pro-104.local>
2025-10-07 18:22:37 +0300

    init project

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      '@': path.resolve(__dirname, 'src'),
+      '@shared': path.resolve(__dirname, '../../packages/shared/src'),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
+
