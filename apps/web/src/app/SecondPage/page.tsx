269e06c60baaffb1e532d57031fe319dfc528633
blue <blue@itmo.ru>
2025-10-08 15:25:29 +0300

    correct

--- apps/web/src/app/SecondPage/page.tsx
+++ apps/web/src/app/SecondPage/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function SecondPage() {
     return (
         <div>
-            <p style={{color: 'blue'}}>Now you are here!</p>
+            <p style={{color: 'red'}}>Now you are here!</p>
             <Link href="/">Back to Home Page</Link>
         </div>
     );
