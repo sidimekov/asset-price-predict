456bc3a59d97f3c9b0cc9f07f331f2a1237f6d06
szamM <szamaxin@gmail.com>
2025-10-30 12:43:44 +0300

    сделал иконки

--- apps/web/src/features/history/HistorySearch.tsx
+++ apps/web/src/features/history/HistorySearch.tsx
@@ -1,20 +1,24 @@
 'use client';
-
+import "../../app/globals.css"
 import { useState } from "react";
 
 export default function SearchBar({ onSearch }: { onSearch: (q: string) => void }) {
   const [query, setQuery] = useState("");
   return (
     <div className="flex gap-2 mb-4">
-      <button className="px-4">лупа</button>
+      <button className="px-4 bg-none ">
+        <img src="/magnifier.svg" alt="Поиск" className="w-5 h-5 "/>
+      </button>
       <input
         type="text"
         placeholder="Search"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="border p-2 rounded-[90px]"
+        className="bg-[#302E53] border-none border p-2 rounded-[90px]"
       />
-      <button className="px-4">фильтр</button>
+      <button className="px-4">
+        <img src="/filter.svg" alt="Поиск" className="w-5 h-5 "/>
+        </button>
 
     </div>
   );
