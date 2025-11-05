6e5129b38df804791be03f8870ccce120d7fab85
studyStudywork <study@MacBook-Pro-134.local>
2025-11-05 03:46:33 +0300

    add features for welcome page

--- apps/web/src/features/landing/TopBar.tsx
+++ apps/web/src/features/landing/TopBar.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import React from 'react';
 import { useRouter } from 'next/navigation';
 import { Button } from '@/shared/ui/Button';
@@ -7,41 +8,29 @@ export const TopBar: React.FC = () => {
   const router = useRouter();
 
   const handleAuthNavigate = (mode: 'signin' | 'signup') => {
-    sessionStorage.setItem('authMode', mode);
-    router.push('/auth');
+    router.push(`/auth?mode=${mode}`);
   };
 
-  React.useEffect(() => {
-    if (window.location.pathname === '/auth') {
-      const mode = sessionStorage.getItem('authMode');
-      if (mode) {
-        const tabButton = document.getElementById(`${mode}-tab`);
-        if (tabButton) (tabButton as any).click();
-        sessionStorage.removeItem('authMode');
-      }
-    }
-  }, []);
-
   return (
-    <header className="w-full flex justify-between items-center py-6 bg-transparent">
-      <h1
-        className="text-4xl font-semibold cursor-pointer font-montserrat select-none "
-        onClick={() => router.push('/')}
-      >
-        <span className="brand-gradient">Asset</span>
-        <span className="text-white">Predict</span>
-      </h1>
-
-      <div className="flex gap-4">
-        <button
-          onClick={() => handleAuthNavigate('signin')}
-          className="btn border min-w-24 text-sm text-white hover:opacity-90 transition"
-          style={{ padding: '10px 16px' }}
+      <header className="w-full flex justify-between items-center py-6 bg-transparent">
+        <h1
+            className="text-4xl font-semibold cursor-pointer font-montserrat select-none"
+            onClick={() => router.push('/')}
         >
-          Sign in
-        </button>
-        <Button onClick={() => handleAuthNavigate('signup')}>Sign up</Button>
-      </div>
-    </header>
+          <span className="brand-gradient">Asset</span>
+          <span className="text-white">Predict</span>
+        </h1>
+
+        <div className="flex gap-4">
+          <button
+              onClick={() => handleAuthNavigate('signin')}
+              className="btn border min-w-24 text-sm text-white hover:opacity-90 transition"
+              style={{ padding: '10px 16px' }}
+          >
+            Sign in
+          </button>
+          <Button onClick={() => handleAuthNavigate('signup')}>Sign up</Button>
+        </div>
+      </header>
   );
 };
