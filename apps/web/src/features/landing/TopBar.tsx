bf05ba61721eed60022a0914c0bf75208565b225
studyStudywork <study@MacBook-Pro-134.local>
2025-11-05 04:02:54 +0300

    add features for welcome page

--- apps/web/src/features/landing/TopBar.tsx
+++ apps/web/src/features/landing/TopBar.tsx
@@ -12,25 +12,25 @@ export const TopBar: React.FC = () => {
   };
 
   return (
-      <header className="w-full flex justify-between items-center py-6 bg-transparent">
-        <h1
-            className="text-4xl font-semibold cursor-pointer font-montserrat select-none"
-            onClick={() => router.push('/')}
-        >
-          <span className="brand-gradient">Asset</span>
-          <span className="text-white">Predict</span>
-        </h1>
+    <header className="w-full flex justify-between items-center py-6 bg-transparent">
+      <h1
+        className="text-4xl font-semibold cursor-pointer font-montserrat select-none"
+        onClick={() => router.push('/')}
+      >
+        <span className="brand-gradient">Asset</span>
+        <span className="text-white">Predict</span>
+      </h1>
 
-        <div className="flex gap-4">
-          <button
-              onClick={() => handleAuthNavigate('signin')}
-              className="btn border min-w-24 text-sm text-white hover:opacity-90 transition"
-              style={{ padding: '10px 16px' }}
-          >
-            Sign in
-          </button>
-          <Button onClick={() => handleAuthNavigate('signup')}>Sign up</Button>
-        </div>
-      </header>
+      <div className="flex gap-4">
+        <button
+          onClick={() => handleAuthNavigate('signin')}
+          className="btn border min-w-24 text-sm text-white hover:opacity-90 transition"
+          style={{ padding: '10px 16px' }}
+        >
+          Sign in
+        </button>
+        <Button onClick={() => handleAuthNavigate('signup')}>Sign up</Button>
+      </div>
+    </header>
   );
 };
