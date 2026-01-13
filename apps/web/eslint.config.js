205a2ed3f04ab7a617d5931d01f9cd99adf35fcb
leias19 <144425202+leias19@users.noreply.github.com>
2026-01-13 22:45:34 +0300

    Удаление излишней таблицы пояснений на /dashboard (#109)
    * удалила таблицу с dashboard
    
    * вроде пофиксила тесты

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -38,6 +38,8 @@ export default [
         HTMLElement: 'readonly',
         require: 'readonly',
         HTMLDivElement: 'readonly',
+        HTMLInputElement: 'readonly',
+        HTMLSelectElement: 'readonly',
       },
     },
     rules: {
