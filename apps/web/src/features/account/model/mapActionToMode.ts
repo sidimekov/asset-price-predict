24b418388719c57cfd1480c527159c15005e78e8
leias19 <liza0272297@yandex.ru>
2026-01-16 02:30:03 +0300

    сопоставление кнопок с режимами модалки

---
+++ apps/web/src/features/account/model/mapActionToMode.ts
@@ -0,0 +1,16 @@
+import type { EditAccountMode } from './editAccountModes';
+
+export const mapActionToMode = (label: string): EditAccountMode | null => {
+  switch (label) {
+    case 'Edit photo':
+      return 'avatar';
+    case 'Change username':
+      return 'username';
+    case 'Change login':
+      return 'login';
+    case 'Change password':
+      return 'password';
+    default:
+      return null;
+  }
+};
